fb84770172b9e91e38c36584e80639102e03b2fc
Praneeth <praneethkacham@gmail.com>
2018-09-22 23:15:21 +0530

    Implement Blending from Perez

--- faceSwapping.py
+++ faceSwapping.py
@@ -63,7 +63,7 @@ class Swapper:
         print((m,n))
         final_image_coordinates = list(itertools.product(range(m),range(n)))
         p = self.tri.find_simplex(final_image_coordinates)
-        print(len(p))
+        # interior_points = {}
         bary_coordinates = self.barycentric_coordinates(final_image_coordinates, p)
         simplices = self.tri.simplices
         final_image = np.zeros((m,n,3))
@@ -79,6 +79,50 @@ class Swapper:
                 final_image[i,j] = 0.7*self.image1_unmod[p1]+0.3*self.image2_unmod[i,j]
         # Now p has the triangle number for each of the pixels
         cv2.imwrite('final.png',final_image)
+
+    def constructIntermediateImageBlending(self):
+        myadd = lambda xs,ys: tuple(int(x + y) for x, y in zip(xs, ys))
+        self.inter_image = self.list2
+        print(self.inter_image)
+        self.tri = Delaunay(self.inter_image)
+        # print(tri.simplices)
+        # tri.simplices is a 2d-numpy array kx3 array with indices of points of each simplex in each row
+        m,n,_ = self.i2Shape
+        print((m,n))
+        final_image_coordinates = list(itertools.product(range(m),range(n)))
+        p = self.tri.find_simplex(final_image_coordinates)
+        interior_points = {}
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if p[i*n + j] >= 0:
+                    interior_points[(i,j)] = count
+                    count += 1
+        A = []
+        b = []
+        bary_coordinates = self.barycentric_coordinates(final_image_coordinates, p)
+        simplices = self.tri.simplices
+        int_image1 = np.zeros((m,n,3))
+        for i in range(m):
+            for j in range(n):
+                triangle_number = p[i*n+j]
+                if triangle_number < 0:
+                    int_image1[i,j] = self.image2_unmod[i,j]
+                    continue
+                corner_points = simplices[triangle_number]
+                barycentric = bary_coordinates[i*n+j]
+                p1 = myadd(myadd(tuple(x*barycentric[0] for x in self.list1[corner_points[0]]),tuple(x*barycentric[1] for x in self.list1[corner_points[1]])),tuple(x*barycentric[2] for x in self.list1[corner_points[2]]))
+                int_image1[i,j] = self.image1_unmod[p1]
+        # Now p has the triangle number for each of the pixels
+        border = lambda tup : True if (p[(tup[0]+1)*n + tup[1]] < 0 or p[(tup[0]-1)*n+tup[1]] < 0 or p[tup[0]*n + tup[1]+1] < 0 or p[tup[0]*n + tup[1]-1] < 0) else False
+        for i in range(m):
+            for j in range(n):
+                if p[i*n + j] < 0:
+                    l = [0*len(interior_points)]
+                    l[interior_points[(i,j)]] = 4
+
+        # cv2.imwrite('final.png',final_image)
+
 
 
 
