275cbbb6e0f79e25313d99030aad9d057180f0ee
udit01 <uditjainiitd@gmail.com>
2018-09-24 17:27:00 +0530

    Added code for tranformation

--- filters.py
+++ filters.py
@@ -5,9 +5,8 @@ import argparse
 import itertools
 class Filter:
 
-    def __init__(self, idx):
+    def __init__(self):
         self.init_lists()
-        self.idx = idx
 
     def loadImages(self, path1):
         self.image1 = cv2.imread(path1)
@@ -27,15 +26,55 @@ class Filter:
 
         self.extendLists()
         self.printLists()
-        # self.constructIntermediateLists()
 
-        self.constructFilteredImage(self.idx)
+        self.constructFilteredImage(0)
     
-    def constructFilteredImage(self, index)
+    def constructFilteredImage(self, idx)
         
-        # Transform self.filters(idx) with cv2.effine tranform, 
+        # Transform self.filters(idx) with cv2.effine tranform,
+        B = np.array(self.image1, dtype=np.float) 
 
-        pass
+        rows,cols,ch = self.image1.shape
+
+        # get from points array and marked things
+        # p_1 = self.points[3*idx + 0]
+        # p_2 = self.points[3*idx + 1]
+        # p_3 = self.points[3*idx + 2]
+
+        # Will throw some error if < 3 points
+        # target_p_1 = self.list1[0] 
+        # target_p_2 = self.list1[1] 
+        # target_p_3 = self.list1[2] 
+        
+        # pts1 = np.float32([p_1,p_2,p_3])
+        # pts2 = np.float32([ target_p_1, target_p_2 , target_p_3 ])
+
+        pts1 = np.float32(self.points[ 3*idx : 3*(idx+1) ])
+        pts2 = np.float32(self.list1[ 0 : 3 ])
+
+        M = cv2.getAffineTransform(pts1,pts2)
+
+        # will extract view of (cols, rows)
+        A = cv2.warpAffine(self.filters[idx], M, (cols,rows))
+
+        alpha = self.alphas[idx]
+
+        # background = cv2.imread('road.jpg')
+        # overlay = cv2.imread('traffic sign.png')
+        # rows,cols,channels = overlay.shape
+        # overlay=cv2.addWeighted(background[250:250+rows, 0:0+cols],0.5,overlay,0.5,0)
+        # background[250:250+rows, 0:0+cols ] = overlay
+
+        # A is foreground, B is background
+        output = (A * alpha) + (B * (1-alpha))
+        # output = (A * alpha) + (B)
+
+        cv2.imwrite('filter_'+idx+'.png' , output)
+
+        cv2.imshow('OUTPUT WITH FILTER '+ idx, output)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
 
     def init_lists(self):
         self.list1 = []
@@ -85,89 +124,6 @@ class Filter:
             self.list1.append((y,x))    # because x is column and y is row
             cv2.circle(self.image1, (x,y), 3, (0,255,0), 3)
             cv2.imshow(self.w1, self.image1)
-
-
-    # def extendLists(self):
-    #     # add the i1 and i2 corner coordinates
-    #     # trim both of them to shorter length
-    #     # self.len = len(self.list2) if (len(self.list1) > len(self.list2)) else len(self.list1) 
-    #     # self.list1 = self.list1[:self.len]
-    #     # self.list1.append((0, 0))
-    #     # self.list1.append((0, self.i1Shape[1]))
-    #     # self.list1.append((self.i1Shape[0], 0))
-    #     # self.list1.append((self.i1Shape[0], self.i1Shape[1]))
-    #     # self.len = self.len + 4
-    #     pass
-
-    # def constructIntermediateLists(self):
-    #     k = self.k 
-    #     self.inter_points = [[] for i in range(k)]
-    #     self.inter_sizes  = []
-    #     for i in range(k):
-    #         left_weight = (float(k-i))/(float(k+1))
-    #         right_weight = (float(i+1))/(float(k+1))
-            
-    #         # do down size in INT
-    #         self.inter_sizes.append(((left_weight*(np.array(self.i1Shape).astype(float))) + (right_weight*(np.array(self.i2Shape).astype(float)))).astype(int))
-
-    #         for j in range(self.len):
-    #             self.inter_points[i].append((left_weight*((np.array(self.list1[j]).astype(float))) + right_weight*((np.array(self.list2[j]).astype(float)))).astype(int))  
-            
-    # def constructIntermediateImages(self):
-    #     self.inter_images = [np.zeros(s) for s in self.inter_sizes]
-    #     self.triangulations = []
-    #     for i in range(self.k):
-    #         # print(len(self.inter_points[i]))
-    #         # print(len(self.inter_points[i][0]))
-    #         # print(len(self.inter_points[i][0].shape))
-    #         tri = Delaunay(self.inter_points[i])
-    #         # print(points[tri.simplices])
-    #         self.triangulations.append(tri)
-
-    #         # Use this link for Barycentric coordinates and effine transform
-    #         # https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.spatial.Delaunay.html
-
-    # def barycentric_coordinates(self, coordinates, triangle_numbers):
-    #     bary_coord = []
-    #     for i in range(len(triangle_numbers)):
-    #         b = list(self.tri.transform[triangle_numbers[i],:2].dot(coordinates[i]-self.tri.transform[triangle_numbers[i],2]))
-    #         b.append(1.0 - sum(b))
-    #         bary_coord.append(b)
-    #     return bary_coord
-
-    # def constructIntermediateImage(self):
-    #     w1 = 1.0/(1.0 + self.w)
-    #     w2 = self.w/(1.0 + self.w)
-    #     myadd = lambda xs,ys: tuple(int(x + y) for x, y in zip(xs, ys))
-    #     self.inter_image = [myadd(tuple(w1*i for i in x) , tuple(w2*i for i in y)) for x,y in zip(self.list1, self.list2)]
-    #     print(self.inter_image)
-    #     self.tri = Delaunay(self.inter_image)
-    #     # print(tri.simplices)
-    #     # tri.simplices is a 2d-numpy array kx3 array with indices of points of each simplex in each row
-    #     m,n = self.inter_image[-1]
-    #     final_image_coordinates = list(itertools.product(range(m),range(n)))
-    #     p = self.tri.find_simplex(final_image_coordinates)
-    #     bary_coordinates = self.barycentric_coordinates(final_image_coordinates, p)
-    #     simplices = self.tri.simplices
-    #     final_image = np.zeros((m,n,3))
-    #     for i in range(m):
-    #         for j in range(n):
-    #             triangle_number = p[i*n+j]
-    #             corner_points = simplices[triangle_number]
-    #             barycentric = bary_coordinates[i*n+j]
-    #             p1 = myadd(myadd(tuple(x*barycentric[0] for x in self.list1[corner_points[0]]),tuple(x*barycentric[1] for x in self.list1[corner_points[1]])),tuple(x*barycentric[2] for x in self.list1[corner_points[2]]))
-    #             p2 = myadd(myadd(tuple(x*barycentric[0] for x in self.list2[corner_points[0]]),tuple(x*barycentric[1] for x in self.list2[corner_points[1]])),tuple(x*barycentric[2] for x in self.list2[corner_points[2]]))
-    #             final_image[i,j] = [int(x) for x in self.image1_unmod[p1]*w1 + self.image2_unmod[p2]*w2]
-    #     # Now p has the triangle number for each of the pixels
-    #     cv2.imwrite('final.png',final_image)
-
-    
-    # def showTriangulation(self):
-    #     pass
-    #     # import matplotlib.pyplot as plt
-    #     # plt.triplot(points[:,0], points[:,1], tri.simplices.copy())
-    #     # plt.plot(points[:,0], points[:,1], 'o')
-    #     # plt.show()
 
     def printLists(self):
         print("------------- Current list--------------")
